404b4fe4299f7752557960861f5a3ddf09aebf9e
mdevlin14 <mdevlin14@gmail.com>
2017-11-28 11:03:15 -0600

    render more results

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -21,9 +21,9 @@ class Profile extends Component {
       return(
         <section className="github-profile">
           <div className="github-profile-info">
-            <h3>{`Username: ${userdata.username}`}</h3>
-            <h3>{`Repo Name: ${userdata.reponame}`}</h3>
-            <h3>{`Stars: ${userdata.stars}`}</h3>
+            <h3>{`Username: ${userdata.owner.login}`}</h3>
+            <h3>{`Repo Name: ${userdata.name}`}</h3>
+            <h3>{`Stars: ${userdata.stargazers_count}`}</h3>
             <h3>{`Score: ${userdata.score}`}</h3>
             <h3>{`Language: ${userdata.language}`}</h3>
             <h3>{`Description: ${userdata.description}`}</h3>
